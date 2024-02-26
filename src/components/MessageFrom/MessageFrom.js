d0e2b4e0b19dbb258e2c977da56660c99760b9f7
seolsis <seoljw1005@naver.com>
2024-02-26 20:36:15 +0900

    feat: relation 컨테이너 UI 완료

--- src/components/MessageFrom/MessageFrom.js
+++ src/components/MessageFrom/MessageFrom.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useRef } from "react";
+import Select from "react-select";
+
+import { Editor } from "@toast-ui/react-editor";
 
 import Frame from "../../assets/Frame.png";
 
 import style from "./MessageFrom.module.css";
 
 function MessageFrom() {
+  const options = [
+    { value: "친구", label: "친구" },
+    { value: "지인", label: "지인" },
+    { value: "동료", label: "동료" },
+    { value: "가족", label: "가족" },
+  ];
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      width: "320px",
+      height: "50px",
+    }),
+  };
+
+  const editorRef = useRef();
+
+  const onChangeGetHTML = () => {
+    // 에디터에 입력된 내용을 HTML 태그 형태로 취득
+    const data = editorRef.current.getInstance().getHTML();
+    // Body에 담기
+    setBody(data);
+  };
+
   return (
     <>
       <div className={style.BigContainer}>
@@ -46,6 +73,15 @@ function MessageFrom() {
               </div>
             </div>
           </div>
+        </div>
+        <div className={style.RelationContainer}>
+          <div className={style.Title}>상대와의 관계</div>
+          <Select
+            placeholder="상대와의 관계를 선택해주세요."
+            options={options}
+            className={style.SelectBox}
+            styles={customStyles}
+          ></Select>
         </div>
       </div>
     </>
