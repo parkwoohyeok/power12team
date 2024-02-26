3d6d6f0dcdd3cbb1cb8c099f8faff0588b484648
seolsis <seoljw1005@naver.com>
2024-02-26 21:01:12 +0900

    feat: 폰트선택 컨테이너 완성

--- src/components/MessageFrom/MessageFrom.js
+++ src/components/MessageFrom/MessageFrom.js
@@ -17,67 +17,80 @@ function MessageFrom() {
     { value: "가족", label: "가족" },
   ];
 
+  const fonts = [
+    { value: "Noto Sans", label: "Noto Sans" },
+    { value: "Pretendard", label: "Pretendard" },
+  ];
+
   return (
     <>
-      <div className={style.BigContainer}>
-        <div className={style.NameContainer}>
-          <div className={style.Title}>From.</div>
-          <input
-            className={style.NameInput}
-            placeholder="이름을 입력해 주세요."
-          />
-        </div>
-        <div className={style.ProfileContainer}>
-          <div className={style.Title}>프로필 이미지</div>
-          <div className={style.Content}>
-            <img
-              className={style.DefaultProfile}
-              src={Frame}
-              alt="기본프로필"
-            ></img>
-            <div className={style.SelectProfile}>
-              <div className={style.SelectExplain}>
-                프로필 이미지를 선택해주세요!
-              </div>
-              <div className={style.SelectImgs}>
-                <img
-                  src={Frame}
-                  alt="이미지선택"
-                  className={style.SelectImg}
-                ></img>
-                <img
-                  src={Frame}
-                  alt="이미지선택"
-                  className={style.SelectImg}
-                ></img>
-                <img
-                  src={Frame}
-                  alt="이미지선택"
-                  className={style.SelectImg}
-                ></img>
+      <div className={style.ParentContainer}>
+        <div className={style.BigContainer}>
+          <div className={style.NameContainer}>
+            <div className={style.Title}>From.</div>
+            <input
+              className={style.NameInput}
+              placeholder="이름을 입력해 주세요."
+            />
+          </div>
+          <div className={style.ProfileContainer}>
+            <div className={style.Title}>프로필 이미지</div>
+            <div className={style.Content}>
+              <img
+                className={style.DefaultProfile}
+                src={Frame}
+                alt="기본프로필"
+              ></img>
+              <div className={style.SelectProfile}>
+                <div className={style.SelectExplain}>
+                  프로필 이미지를 선택해주세요!
+                </div>
+                <div className={style.SelectImgs}>
+                  <img
+                    src={Frame}
+                    alt="이미지선택"
+                    className={style.SelectImg}
+                  ></img>
+                  <img
+                    src={Frame}
+                    alt="이미지선택"
+                    className={style.SelectImg}
+                  ></img>
+                  <img
+                    src={Frame}
+                    alt="이미지선택"
+                    className={style.SelectImg}
+                  ></img>
+                </div>
               </div>
             </div>
           </div>
+          <div className={style.RelationContainer}>
+            <div className={style.Title}>상대와의 관계</div>
+            <Select
+              placeholder="상대와의 관계를 선택해주세요."
+              options={options}
+              className={style.SelectBox}
+            ></Select>
+          </div>
+          <div className={style.ContentContainer}>
+            <div className={style.Title}>내용을 입력해 주세요</div>
+            <Editor
+              value={text}
+              onTextChange={(e) => setText(e.htmlValue)}
+              style={{ height: "320px" }}
+            />
+          </div>
+          <div className={style.FontContainer}>
+            <div className={style.Title}>폰트 선택</div>
+            <Select
+              options={fonts}
+              className={style.FontSelectBox}
+              placeholder="사용할 폰트를 선택해주세요."
+            ></Select>
+          </div>
         </div>
-        <div className={style.RelationContainer}>
-          <div className={style.Title}>상대와의 관계</div>
-          <Select
-            placeholder="상대와의 관계를 선택해주세요."
-            options={options}
-            className={style.SelectBox}
-          ></Select>
-        </div>
-        <div className={style.ContentContainer}>
-          <div className={style.Title}>내용을 입력해 주세요</div>
-          <Editor
-            value={text}
-            onTextChange={(e) => setText(e.htmlValue)}
-            style={{ width: "720px", height: "320px" }}
-          />
-        </div>
-        <div className={style.FontContainer}>
-          <div className={style.Title}>폰트 선택</div>
-        </div>
+        <button className={style.Button}></button>
       </div>
     </>
   );
