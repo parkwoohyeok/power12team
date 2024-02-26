7ea543d867263ff5a2de05773228acd72cca6014
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-02-26 21:12:57 +0900

    feature: 롤링 페이퍼 작성자 수 표시 컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 /* eslint-disable */
 
 import HotList from "components/HotList/HotList";
+import RecentCount from "components/RecentCount/RecentCount";
+import RecentMessage from "components/RecentMessage/RecentMessage";
 
 function App() {
   return (
     <>
       <HotList />
+      <RecentMessage />
     </>
   );
 }
