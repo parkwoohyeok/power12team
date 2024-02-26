88dc0ead0e2e24aa8af844eebb4990ed9b76cf0e
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-02-26 22:11:41 +0900

    feat: PostPage UI 구현

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PostPage from "./components/PostPage/PostPage";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
