768190a81f1fbbf0a6f0c85aac364a077e140fca
seolsis <seoljw1005@naver.com>
2024-02-26 19:25:30 +0900

    feat: From, 프로필 이미지 구현

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
+/* eslint-disabled */
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import MessageFrom from "./components/MessageFrom/MessageFrom";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/post" element={<MessageFrom />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/MessageFrom/MessageFrom.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Frame from "../../assets/Frame.png";
+
+import style from "./MessageFrom.module.css";
+
+function MessageFrom() {
+  return (
+    <>
+      <div className={style.BigContainer}>
+        <div className={style.NameContainer}>
+          <div className={style.Title}>From.</div>
+          <input
+            className={style.NameInput}
+            placeholder="이름을 입력해 주세요."
+          />
+        </div>
+        <div className={style.ProfileContainer}>
+          <div className={style.Title}>프로필 이미지</div>
+          <div className={style.Content}>
+            <img
+              className={style.DefaultProfile}
+              src={Frame}
+              alt="기본프로필"
+            ></img>
+            <div className={style.SelectProfile}>
+              <div className={style.SelectExplain}>
+                프로필 이미지를 선택해주세요!
+              </div>
+              <div className={style.SelectImg}></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MessageFrom;
