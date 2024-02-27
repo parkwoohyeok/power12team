08574ae5eb58dcc07082e8f61d42773c51c49908
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-02-27 13:06:09 +0900

    style: 포스트 페이지(to) 컴포넌트 이름 변경

---
+++ src/components/MessageTo/MessageTo.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import styles from "./MessageTo.module.css";
+
+const MessageTo = () => {
+  const [color, setColor] = useState();
+  const [image, setImage] = useState();
+  return (
+    <div className={styles.PostPageBody}>
+      <div className={styles.InputPost}>
+        <span className={styles.InputPostText}>To.</span>
+        <input placeholder="받는 사람 이름을 입력해 주세요"></input>
+      </div>
+      <div className={styles.Center}>
+        <div className={styles.ChooseText}>
+          <div className={styles.ChooseView}>배경화면을 선택해 주세요.</div>
+          <div className={styles.ChooseColorImage}>
+            컬러를 선택하거나, 이미지를 선택할 수 있습니다.
+          </div>
+        </div>
+        <div className={styles.ChooseOptionButton}>
+          <button
+            className={styles.ChooseButton}
+            value={color}
+            setColor={setColor}
+          >
+            컬러
+          </button>
+          <button
+            className={styles.ChooseButton}
+            value={image}
+            setImage={setImage}
+          >
+            이미지
+          </button>
+        </div>
+      </div>
+      <div className={styles.Colors}>
+        <div className={styles.Color}></div>
+        <div className={styles.Color}></div>
+        <div className={styles.Color}></div>
+        <div className={styles.Color}></div>
+      </div>
+      <div className={styles.ButtonSpace}>
+        <button className={styles.CreateButton}>생성하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default MessageTo;
