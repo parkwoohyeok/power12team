ddd99d877e3663819518e1298d6167ff1fb36145
parkwoohyeok <dngur3111@naver.com>
2024-02-27 17:18:49 +0900

    feat: 페이지네이션 및 슬라이드 구현

--- src/components/HotList/HotList.js
+++ src/components/HotList/HotList.js
@@ -8,20 +8,48 @@ import data from "mock/mock.json";
 
 import arrow from "assets/arrow.png";
 
+import MessageSummary from "components/MessageSummary/MessageSummary";
+import { useState } from "react";
+
 function HotList() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardSlidingToRight, setCardSlidingToRight] = useState(false);
+  const [cardSlidingToLeft, setCardSlidingToLeft] = useState(false);
+  const ArrayData = [...data];
+  const CARDS_PER_PAGE = 4;
+  const totalPages = Math.ceil(ArrayData.length / CARDS_PER_PAGE);
+  const startIndex = (currentPage - 1) * CARDS_PER_PAGE;
+  const endIndex = Math.min(startIndex + CARDS_PER_PAGE, ArrayData.length);
+  const HotCards = ArrayData.slice(startIndex, endIndex);
+
+  const nextPage = () => {
+    setCardSlidingToRight(true);
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    setTimeout(() => {
+      setCardSlidingToRight(false);
+    }, 200);
+  };
+
+  const prevPage = () => {
+    setCardSlidingToLeft(true);
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    setTimeout(() => {
+      setCardSlidingToLeft(false);
+    }, 200);
+  };
+
   return (
     <>
       <h2 className={styles.CardTitle}>인기 롤링 페이퍼🔥</h2>
-      <div className={styles.Wrapper}>
-        {data.map((info) => (
+      <div layout className={styles.Wrapper}>
+        {HotCards.map((info) => (
           <div
             key={info.id}
-            className={`${styles["CardContainer"]} ${styles[info.backgroundColor]} `}
+            className={`${styles["CardContainer"]} ${styles[info.backgroundColor]} ${cardSlidingToRight ? styles["slide-out-R"] : ""} ${cardSlidingToLeft ? styles["slide-out-L"] : ""}`}
           >
             <div className={styles["CardInfo"]}>
               <h2>{`To.${info.name}`}</h2>
-              <h2>이미지 컴포넌트</h2>
-              <h2>인원수 컴포넌트</h2>
+              <MessageSummary />
             </div>
             <div className={styles.CardFooter}>
               <div className={styles.HorizonLine}></div>
@@ -29,10 +57,16 @@ function HotList() {
             </div>
           </div>
         ))}
-        <button className={styles.SlideBtn_R}>
+        <button
+          className={`${styles.SlideBtn_R} ${currentPage === totalPages ? styles.EndOfPage : ""}`}
+          onClick={nextPage}
+        >
           <img src={arrow} alt="슬라이드 버튼" />
         </button>
-        <button className={styles.SlideBtn_L}>
+        <button
+          className={`${styles.SlideBtn_L} ${currentPage === 1 ? styles.EndOfPage : ""}`}
+          onClick={prevPage}
+        >
           <img src={arrow} alt="슬라이드 버튼" />
         </button>
       </div>
--- src/components/RecentList/RecentList.js
+++ src/components/RecentList/RecentList.js
@@ -7,21 +7,54 @@ import styles from "components/RecentList/RecentList.module.css";
 import data from "mock/mock.json";
 
 import arrow from "assets/arrow.png";
+import MessageSummary from "components/MessageSummary/MessageSummary";
+import { useState } from "react";
 
 const RecentList = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [cardSlidingToRight, setCardSlidingToRight] = useState(false);
+  const [cardSlidingToLeft, setCardSlidingToLeft] = useState(false);
+
+  const sortedData = [...data].sort((a, b) => {
+    const timeA = new Date(a.createdAt).getTime();
+    const timeB = new Date(b.createdAt).getTime();
+    return timeB - timeA;
+  });
+
+  const CARDS_PER_PAGE = 4;
+  const totalPages = Math.ceil(data.length / CARDS_PER_PAGE);
+  const startIndex = (currentPage - 1) * CARDS_PER_PAGE;
+  const endIndex = Math.min(startIndex + CARDS_PER_PAGE, data.length);
+  const currentCards = sortedData.slice(startIndex, endIndex);
+
+  const nextPage = () => {
+    setCardSlidingToRight(true);
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+    setTimeout(() => {
+      setCardSlidingToRight(false);
+    }, 200);
+  };
+
+  const prevPage = () => {
+    setCardSlidingToLeft(true);
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+    setTimeout(() => {
+      setCardSlidingToLeft(false);
+    }, 200);
+  };
+
   return (
     <>
       <h2 className={styles.CardTitle}>최근에 만든 롤링 페이퍼⭐</h2>
       <div className={styles.Wrapper}>
-        {data.map((info) => (
+        {currentCards.map((info) => (
           <div
             key={info.id}
-            className={`${styles["CardContainer"]} ${styles[info.backgroundColor]} `}
+            className={`${styles["CardContainer"]} ${styles[info.backgroundColor]} ${cardSlidingToRight ? styles["slide-out-R"] : ""} ${cardSlidingToLeft ? styles["slide-out-L"] : ""} `}
           >
             <div className={styles["CardInfo"]}>
               <h2>{`To.${info.name}`}</h2>
-              <h2>이미지 컴포넌트</h2>
-              <h2>인원수 컴포넌트</h2>
+              <MessageSummary />
             </div>
             <div className={styles.CardFooter}>
               <div className={styles.HorizonLine}></div>
@@ -29,10 +62,17 @@ const RecentList = () => {
             </div>
           </div>
         ))}
-        <button className={styles.SlideBtn_R}>
+        <button
+          className={`${styles.SlideBtn_R} ${currentPage === totalPages ? styles.EndOfPage : ""}
+        `}
+          onClick={nextPage}
+        >
           <img src={arrow} alt="슬라이드 버튼" />
         </button>
-        <button className={styles.SlideBtn_L}>
+        <button
+          className={`${styles.SlideBtn_L} ${currentPage === 1 ? styles.EndOfPage : ""}`}
+          onClick={prevPage}
+        >
           <img src={arrow} alt="슬라이드 버튼" />
         </button>
       </div>
