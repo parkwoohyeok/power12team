4ee42dae424a9046c4fb56ea45895b63efc60727
seolsis <seoljw1005@naver.com>
2024-02-28 14:59:43 +0900

    feat: 버튼상태 비활성화

--- src/components/MessageFrom/MessageFrom.js
+++ src/components/MessageFrom/MessageFrom.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Select from "react-select";
 
 import { Editor } from "primereact/editor";
@@ -9,10 +9,13 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 import styles from "./MessageFrom.module.css";
 
 function MessageFrom() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState(""); // 사용자가 입력한 메시지 값 저장
   const [name, setName] = useState(""); // 사용자가 입력한 이름 값 저장
   const [inputError, setInputError] = useState(""); // 입력에 대한 에러 메시지 저장
   const [profileImage, setProfileImage] = useState(null); // 사용자가 선택한 프로필 이미지 저장
+
+  // 생성하기 버튼 활성화 조건 검사 함수
+  const isButtonEnabled = name?.trim() !== "" && text?.trim() !== "";
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -24,6 +27,10 @@ function MessageFrom() {
       setProfileImage(URL.createObjectURL(e.target.files[0]));
     }
   };
+
+  useEffect(() => {
+    console.log(`버튼 활성화 상태: ${isButtonEnabled}`);
+  }, [name, text]);
 
   const customStyles = {
     control: (base) => ({
@@ -62,6 +69,7 @@ function MessageFrom() {
   const defaultRelationValue = options.find(
     (option) => option.value === "지인",
   );
+
   return (
     <>
       <div className={styles.ParentContainer}>
@@ -127,7 +135,7 @@ function MessageFrom() {
             <div className={styles.Title}>내용을 입력해 주세요</div>
             <Editor
               value={text}
-              onTextChange={(e) => setText(e.htmlValue)}
+              onTextChange={(e) => setText(e.value)}
               styles={{ height: "320px" }}
             />
           </div>
@@ -143,7 +151,9 @@ function MessageFrom() {
           </div>
         </div>
         <div className={styles.Bottom}>
-          <button className={styles.Button}>생성하기</button>
+          <button className={styles.Button} disabled={!isButtonEnabled}>
+            생성하기
+          </button>
         </div>
       </div>
     </>
