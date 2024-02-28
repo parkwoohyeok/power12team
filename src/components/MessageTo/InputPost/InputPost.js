2d044399329369abc50eaa108e5bd2dee2f81ee8
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-02-28 15:25:32 +0900

    feat: input 에러 메세지 기능 추가

--- src/components/MessageTo/InputPost/InputPost.js
+++ src/components/MessageTo/InputPost/InputPost.js
@@ -1,10 +1,37 @@
+import { useState } from "react";
+
 import styles from "./InputPost.module.css";
 
-const InputPost = () => (
-  <div className={styles.InputPost}>
-    <span className={styles.InputPostText}>To.</span>
-    <input placeholder="받는 사람 이름을 입력해 주세요"></input>
-  </div>
-);
+const InputPost = () => {
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+
+  const onChange = (e) => {
+    setName(e.target.value);
+    setError(false);
+  };
+
+  const handleError = () => {
+    if (!name) {
+      setError(true);
+    }
+  };
+
+  return (
+    <div className={styles.InputPost}>
+      <span className={styles.InputPostText}>To.</span>
+      <input
+        className={`${styles.InputBox} ${error ? styles.InputError : ""}`}
+        onChange={onChange}
+        value={name}
+        onBlur={handleError}
+        placeholder="받는 사람 이름을 입력해 주세요"
+      ></input>
+      {error && (
+        <div className={styles.InputErrorMessage}>값을 입력해 주세요.</div>
+      )}
+    </div>
+  );
+};
 
 export default InputPost;
