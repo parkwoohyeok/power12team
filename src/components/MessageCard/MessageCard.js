227b0b4e8c06987885957adf4cd266f0b2855bc7
hanbin <rj2303@naver.com>
2024-02-28 09:41:37 +0900

    feat: 메세지 모달 열림 기능 구현

--- src/components/MessageCard/MessageCard.js
+++ src/components/MessageCard/MessageCard.js
@@ -1,17 +1,26 @@
+import { useState } from "react";
+
 import mockData from "../../assets/mock.json";
 import MessageCardContainer from "../MessageCardContainer/MessageCardContainer";
+import MessageCardModal from "../MessageCardModal/MessageCardModal";
 import RelationshipTag from "../RelationshipTag/RelationshipTag";
 
 import styles from "./MessageCard.module.css";
 
 const MessageCard = ({ message }) => {
+  const [messageCardModalOpen, setMessageCardModalOpen] = useState(false);
+
   const CREATED_AT = new Date(mockData.createdAt);
   const CREATED_YEAR = CREATED_AT.getFullYear();
   const CREATED_MONTH = CREATED_AT.getMonth();
   const CREATED_DATE = CREATED_AT.getDate();
 
+  const handleClick = () => {
+    setMessageCardModalOpen(true);
+  };
+
   return (
-    <MessageCardContainer>
+    <MessageCardContainer onClick={handleClick}>
       <div className={styles.SenderInfo}>
         <img className={styles.SenderProfile} src={message.profileImageURL} />
         <div className={styles.SenderText}>
@@ -27,6 +36,12 @@ const MessageCard = ({ message }) => {
       <p
         className={styles.CreatedAt}
       >{`${CREATED_YEAR}.${CREATED_MONTH}.${CREATED_DATE}`}</p>
+      {messageCardModalOpen && (
+        <MessageCardModal
+          message={message}
+          setMessageCardModalOpen={setMessageCardModalOpen}
+        />
+      )}
     </MessageCardContainer>
   );
 };
---
+++ src/components/MessageCardModal/MessageCardModal.js
@@ -0,0 +1,11 @@
+import styles from "./MessageCardModal.module.css";
+
+const MessageCardModal = () => {
+  return (
+    <div className={styles.ModalBackground}>
+      <div className={styles.MessageModalBox}>hello</div>
+    </div>
+  );
+};
+
+export default MessageCardModal;
