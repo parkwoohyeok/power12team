924de1198b49bcfd403f0dbff074cbb2fe8d4844
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-02-29 19:02:45 +0900

    feat: LandingPage UI구현

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -23,13 +23,15 @@ const Nav = () => {
   console.log(recipientData);
 
   return (
-    <div className={styles.Container}>
-      <button className={styles.LogoBtn}>
-        <img src={logo} alt="네브바 로고" />
-      </button>
-      <button className={styles.GoToPostPage}>
-        <span>롤링 페이퍼 만들기</span>
-      </button>
+    <div className={styles.NavBar}>
+      <div className={styles.Container}>
+        <button className={styles.LogoBtn}>
+          <img src={logo} alt="네브바 로고" />
+        </button>
+        <button className={styles.GoToPostPage}>
+          <span>롤링 페이퍼 만들기</span>
+        </button>
+      </div>
     </div>
   );
 };
