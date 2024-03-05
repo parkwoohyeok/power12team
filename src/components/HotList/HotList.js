9124521fff3b75c4c1932a80b1ae8d4162c9130a
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-03-05 13:23:48 +0900

    feat: 카드 리스트 배경 css 클래스 수정

--- src/components/HotList/HotList.js
+++ src/components/HotList/HotList.js
@@ -92,7 +92,7 @@ function HotList() {
                 : {}
             }
             key={info.id}
-            className={`${styles["CardContainer"]} ${info.backgroundColor ? styles[info.backgroundColor] : ""}  ${cardSlidingToRight ? styles["slide-out-R"] : ""} ${cardSlidingToLeft ? styles["slide-out-L"] : ""}`}
+            className={`${styles["CardContainer"]} ${info.backgroundImageURL ? "" : styles[info.backgroundColor]} ${cardSlidingToRight ? styles["slide-out-R"] : ""} ${cardSlidingToLeft ? styles["slide-out-L"] : ""}`}
           >
             <div className={styles["CardInfo"]}>
               <h2>{`To.${info.name}`}</h2>
