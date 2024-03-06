3c2a2343bcbd4bafc100e6f563676aa95318497c
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-03-06 20:12:04 +0900

    refactor: Error 메시지 수정

--- src/components/MessageTo/MessageTo.js
+++ src/components/MessageTo/MessageTo.js
@@ -47,7 +47,7 @@ const MessageTo = () => {
         navigate(`${v.id}`);
       });
     } catch (error) {
-      throw new Error("Error in useEffect:", error);
+      throw new Error("Error fetching data:", error);
     }
   };
 
