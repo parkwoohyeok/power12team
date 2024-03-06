cdcc5928b54c757dec015c5f37e222d1dd1fc721
hanbin <rj2303@naver.com>
2024-03-06 15:54:13 +0900

    refactor: axios 객체 적용

--- src/components/Api/RecipientApi.js
+++ src/components/Api/RecipientApi.js
@@ -1,10 +1,8 @@
-import axios from "axios";
-
-const BASE_URL = "https://rolling-api.vercel.app";
+import axiosInstance from "../../utils/axiosInstance";
 
 export const getRecipient = async (recipientId) => {
-  const RESPONSE = await axios
-    .get(`${BASE_URL}/4-12/recipients/${recipientId}/`)
+  const RESPONSE = await axiosInstance
+    .get(`recipients/${recipientId}/`)
     .catch((error) => {
       throw new Error("사용자 정보를 받아오는데 실패했어요.");
     });
@@ -13,10 +11,8 @@ export const getRecipient = async (recipientId) => {
 };
 
 export const getMessages = async ({ recipientId, limit, offset }) => {
-  const RESPONSE = await axios
-    .get(
-      `${BASE_URL}/4-12/recipients/${recipientId}/messages/?limit=${limit}&offset=${offset}`,
-    )
+  const RESPONSE = await axiosInstance
+    .get(`recipients/${recipientId}/messages/?limit=${limit}&offset=${offset}`)
     .catch((error) => {
       throw new Error("메세지 목록을 받아오는데 실패했어요.");
     });
@@ -25,8 +21,8 @@ export const getMessages = async ({ recipientId, limit, offset }) => {
 };
 
 export const deleteMessage = async (id) => {
-  const RESPONSE = await axios
-    .delete(`${BASE_URL}/4-12/messages/${id}/`)
+  const RESPONSE = await axiosInstance
+    .delete(`messages/${id}/`)
     .catch((error) => {
       throw new Error("메세지가 존재하지 않거나 지워지지 않았어요.");
     });
