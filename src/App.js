3bd74d17b0a51e6c67bb3003e0cde1c7209fee05
hanbin <rj2303@naver.com>
2024-03-10 21:01:28 +0900

    fix: 파일 구조 수정

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import ListPage from "./pages/ListPage/ListPage";
+import MessagePage from "./pages/MessagePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import PostPage from "./pages/PostPage";
+import RecipientPage from "./pages/RecipientPage/RecipientPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="list" element={<ListPage />} />
+        <Route path="post">
+          <Route index element={<PostPage />} />
+          <Route path=":recipientId">
+            <Route index element={<RecipientPage />} />
+            <Route path="message" element={<MessagePage />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
