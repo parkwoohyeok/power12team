5cd6ba24322999f5df733e2aab4ed0afee87dada
hanbin <rj2303@naver.com>
2024-03-13 18:49:30 +0900

    feat: 메세지 카드마다 글꼴 적용

--- src/components/RecipientPage/MessageCardList/MessageCard/MessageCard.js
+++ src/components/RecipientPage/MessageCardList/MessageCard/MessageCard.js
@@ -1,12 +1,15 @@
 import styles from "./MessageCard.module.css";
 
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import MessageCardModal from "../../MessageCardModal/MessageCardModal";
 import MessageCardContainer from "../MessageCardContainer/MessageCardContainer";
 import CreatedAt from "./CreatedAt/CreatedAt";
 import SenderInfo from "./SenderInfo/SenderInfo";
 
 const MessageCard = ({ message, isEditing, onDelete }) => {
+  const CONTENT_ELEMENT = useRef();
+  const FONT = message.font.replaceAll(" ", "");
+
   const [messageCardModalOpen, setMessageCardModalOpen] = useState(false);
 
   const handleClick = () => {
@@ -15,14 +18,20 @@ const MessageCard = ({ message, isEditing, onDelete }) => {
     }
   };
 
+  useEffect(() => {
+    const CONTENT = message.content;
+    CONTENT_ELEMENT.current.innerHTML = CONTENT;
+  }, []);
+
   return (
     <MessageCardContainer onClick={handleClick}>
       <SenderInfo message={message} isEditing={isEditing} onDelete={onDelete} />
       <div className={styles.DividingLine}></div>
       <div
-        className={styles.CardContent}
-        dangerouslySetInnerHTML={{ __html: message.content }}
-      />
+        ref={CONTENT_ELEMENT}
+        className={`${styles.CardContent} ${styles[FONT]}`}
+        // dangerouslySetInnerHTML={{ __html: message.content }}
+      ></div>
       <CreatedAt createdAt={message.createdAt} />
       {messageCardModalOpen && (
         <MessageCardModal
